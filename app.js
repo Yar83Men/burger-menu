afd7f4c66c6937130792c9248f0ac1fc0d4ba36c
Yaroslaw Menda <yarik83.evpat@gmail.com>
2022-05-21 11:39:47 +0300

    burger menu

---
+++ app.js
@@ -0,0 +1,11 @@
+const button = document.querySelector(".trigger");
+const menu = document.querySelector(".full-screen-nav");
+const container = document.querySelector(".container");
+
+button.onclick = () => {
+    menu.classList.add("open");    
+}
+
+container.onclick = () => {
+    menu.classList.remove("open");
+}
